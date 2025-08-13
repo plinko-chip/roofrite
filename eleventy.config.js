db3d14f07fcb56de995f2dc290d983a0c6169ac8
jskinne3 <jno.skinner@gmail.com>
2025-08-12 18:46:04 -0700

    Add image for 404 page

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,15 +5,13 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets/img/*.jpg");
   eleventyConfig.addPassthroughCopy("assets/img/*.jpeg");
   eleventyConfig.addPassthroughCopy("assets/img/*.svg");
+  eleventyConfig.addPassthroughCopy("assets/img/*.gif");
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("assets/img/badges/*.svg");
-
+  
   /* other asset types */
   eleventyConfig.addPassthroughCopy("assets/css/*.css");
   eleventyConfig.addPassthroughCopy("assets/vid/*.webm");
-
-  /* why? */
-  eleventyConfig.addPassthroughCopy("404.html");
 
   return {};
 };
